58901fd3ea17f5c1c4e2fab72b4d5ff937bbcb92
jihaniqua <jdmallonga@gmail.com>
2023-03-03 01:35:14 -0500

    post page changes

--- posts.php
+++ posts.php
@@ -4,49 +4,68 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/app.css" rel="stylesheet" />
     <title>Posts</title>
+    <!-- normalize to remove browser default styles -->
+    <link rel="stylesheet" href="css/normalize.css" />
+    <!-- our custom css -->
+    <link rel="stylesheet" href="css/app.css" />
+    <!-- our custom js -->
+    <script src="js/scripts.js" defer></script>
 </head>
+
 <body>
-    <h1>Posts</h1>
-    <?php
-    // connect to db
-    $db = new PDO('mysql:host=172.31.22.43;dbname=Jihan200523101', 'Jihan200523101', 'ZjTmwnJCwo');
+    <header>
+        <h1>
+            <a href="#">
+                MediaCon
+            </a>
+        </h1>
+        <nav>
+            <ul>
+                <li><a href="posts.php">Posts</a></li>
+                <li><a href="register.php">Register</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>Posts</h1>
+        <a href="post-details.php">Add a New Post</a>
+        <?php
+        // connect to db
+        $db = new PDO('mysql:host=172.31.22.43;dbname=Jihan200523101', 'Jihan200523101', 'ZjTmwnJCwo');
+        // set up the SQL SELECT command
+        $sql = "SELECT * FROM posts ORDER BY postId DESC";
+        // execute the select query
+        $cmd = $db->prepare($sql);
+        $cmd->execute();
+        // store the query results in an array. use fetchAll for multiple records, fetch for 1.
+        $posts = $cmd->fetchAll();
+        /*echo '<table>
+        <thead><th>Body</th><th>User</th><th>Date</th></thead>';*/
+        // display post data in a loop. $posts = all data, $post = the current item in the loop
+        foreach ($posts as $post) {
+            echo '<article>
+            <h2>' . $post['user'] . '</h2>
+            <p>' . $post['dateCreated'] . '</p>
+            <p>' . $post['body'] . '</p>
+            <a href="edit-post.php?postId=' . $post['postId'] . '">Edit</a>
+            <a onclick="return confirmDelete();"
+             href="delete-post.php?postId=' . $post['postId'] .'
+            ">Delete</a>
+            </article>';
 
-    // set up the SQL SELECT command
-    // Query is using select because we're retrieving data instead of inserting where we were sending data to the database
-    $sql = "SELECT * FROM posts";
-
-    // execute the select query
-    // command variable, prepare to run our query
-    $cmd = $db->prepare($sql);
-    // if we don't call execute, we've connected and set up a query but the query never happens
-    $cmd->execute();
-
-    // store the query results in an array. Use fetchAll for multiple records, fetch for 1.
-    $posts = $cmd->fetchAll();
-
-    // open table
-    // echo '<table>';
-    // echo '<thead><th>Body</th><th>User</th><th>Date</th>';
-    //simplified
-    echo '<table>
-        <thead><th>Body</th><th>User</th><th>Date</th>';
-
-    // display the post data in a loop. $posts = all the data, $post = current item in the loop
-    foreach ($posts as $post) {
-        echo '<tr>'; // create a new row
-        echo '<td>' . $post['body'] . '</td>'; // create new column with data inside
-        echo '<td>' . $post['user'] . '</td>';
-        echo '<td>' . $post['dateCreated'] . '</td>';
-        echo '</tr>';
-    }
-
-    // close table
-    echo '</table>';
-
-    // disconnect
-    $db = null;
-    ?>
+            /*echo '<tr>
+            <td>' . $post['body'] . '</td>
+            <td>' . $post['user']. '</td>
+            <td>' . $post['dateCreated']. '</td>
+        </tr>';*/
+        }
+        // close table
+        //echo '</table>';
+        // disconnect
+        $db = null;
+        ?>
+    </main>
 </body>
 </html>
